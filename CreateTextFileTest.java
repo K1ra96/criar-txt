fac9f40aa8939ce3f198852f9ff5d3076f3549ff
K1ra96 <136936389+K1ra96@users.noreply.github.com>
2024-08-28 14:20:27 -0400

    Create CreateTextFileTest.java

---
+++ CreateTextFileTest.java
@@ -0,0 +1,9 @@
+package javaapplication11;
+public class CreateTextFileTest {
+    public static void main(String[] args){
+        CreateTextFile application = new CreateTextFile();
+        application.openFile();
+        application.addRecords();
+        application.closeFile();
+    }
+}
